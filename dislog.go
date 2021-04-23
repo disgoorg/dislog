bf9a599eedb4864fa590dd9b74854f552b3fa9fd
TopiSenpai <tobi@strasserhof.com>
2021-04-23 18:08:44 +0200

    implemented log colors & embed building

--- dislog.go
+++ dislog.go
@@ -1,10 +1,32 @@
 package dislog
 
 import (
+	"fmt"
 	"github.com/DisgoOrg/disgohook"
 	"github.com/DisgoOrg/disgohook/api"
 	"github.com/sirupsen/logrus"
+	"strconv"
 )
+
+const (
+	PanicLevelColor = 0xe300bd
+	FatalLevelColor = 0xeb4034
+	ErrorLevelColor = 0xff0011
+	WarnLevelColor  = 0xfcec00
+	InfoLevelColor  = 0x002ccc
+	DebugLevelColor = 0x0095ff
+	TraceLevelColor = 0xffffff
+)
+
+var LevelColors = map[logrus.Level]int{
+	logrus.PanicLevel: PanicLevelColor,
+	logrus.FatalLevel: FatalLevelColor,
+	logrus.ErrorLevel: ErrorLevelColor,
+	logrus.WarnLevel:  WarnLevelColor,
+	logrus.InfoLevel:  InfoLevelColor,
+	logrus.DebugLevel: DebugLevelColor,
+	logrus.TraceLevel: TraceLevelColor,
+}
 
 var _ logrus.Hook = (*DisLog)(nil)
 
@@ -13,30 +35,30 @@ func NewDisLogBuilder() *DisLogBuilder {
 }
 
 func NewDisLogByToken(webhookToken string, levels ...logrus.Level) (*DisLog, error) {
-	dgoHook, err := disgohook.NewDisgoHookByToken(webhookToken)
+	webhook, err := disgohook.NewWebhookByToken(webhookToken)
 	if err != nil {
 		return nil, err
 	}
 	return &DisLog{
-		disgoHook: dgoHook,
-		levels:    levels,
+		webhook: webhook,
+		levels:  levels,
 	}, nil
 }
 
 func NewDisLogByID(webhookID string, token string, levels ...logrus.Level) (*DisLog, error) {
-	dgoHook, err := disgohook.NewDisgoHookByID(webhookID, token)
+	webhook, err := disgohook.NewWebhookByID(webhookID, token)
 	if err != nil {
 		return nil, err
 	}
 	return &DisLog{
-		disgoHook: dgoHook,
-		levels:    levels,
+		webhook: webhook,
+		levels:  levels,
 	}, nil
 }
 
 type DisLog struct {
-	disgoHook api.DisgoHook
-	levels    []logrus.Level
+	webhook api.Webhook
+	levels  []logrus.Level
 }
 
 func (l *DisLog) Levels() []logrus.Level {
@@ -44,9 +66,23 @@ func (l *DisLog) Levels() []logrus.Level {
 }
 
 func (l *DisLog) Fire(entry *logrus.Entry) error {
-	_, err := l.disgoHook.SendMessage()
+	embed := api.NewEmbedBuilder().
+		SetColor(LevelColors[entry.Level]).
+		SetTitle("log in file: "+entry.Caller.File+" from func: "+entry.Caller.Function+" in line: "+strconv.Itoa(entry.Caller.Line)).
+		SetDescriptionf(entry.Message).
+		AddField("Level", entry.Level.String(), true).
+		AddField("Time", entry.Time.String(), true)
+	for key, value := range entry.Data {
+		embed.AddField(key, fmt.Sprint(value), true)
+	}
+	_, err := l.webhook.SendMessage(api.NewWebhookMessageWithEmbeds(embed.Build()).Build())
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func subStr(str string, start int, end int) string {
+	a := []rune(str)
+	return string(a[start:end])
 }
